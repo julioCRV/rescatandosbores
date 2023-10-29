5100dbf402f453fa5d35a92802de9679abd08d2f
Ricardo <richardyord@gmail.com>
2023-10-29 12:06:06 +0100

    Modificacion del lazy loadig (Lado del css)

--- src/components/EditarPlatillo/EditarPlatillo.jsx
+++ src/components/EditarPlatillo/EditarPlatillo.jsx
@@ -8,7 +8,6 @@ import './EditarPlatillo.css';
 import { useParams } from 'react-router-dom';
 
 const { Title } = Typography;
-
 const { TextArea } = Input;
 
 export const EditarPlatillo = () =>{
@@ -171,7 +170,6 @@ const onFinish = async (values) => {
     console.log(err);
   } finally{
     setIsLoading(false); //Desactiva la interfaz de carga
-    
   }
 };
 
@@ -182,7 +180,6 @@ const onFinish = async (values) => {
     
     <div className="titulo-formato">Editar Platillo</div  >
     {isLoading  &&  <Spin size='large' className='ant-spin'/>}
-
 {/*       {isLoading && <Spin size="large" style={{ position: 'absolute', top: '50%', left: '50%' }} /> } */} {/* Interfaz de carga */}
 
       <Form.Item className='componente-limite'
