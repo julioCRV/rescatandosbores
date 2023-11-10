3821352fb2fffd1a8e486cbe1b0bd453f0129386
Romulo Tole <tolemaicor@gmail.com>
2023-11-10 01:04:40 -0400

    Definiendo estructura del modulo Estadisticas

---
+++ src/components/Estadisticas/Estadisticas.jsx
@@ -0,0 +1,24 @@
+
+
+const estadisticas = () => {
+    return(
+        <div className="estadistica">
+            <div className="tituloEst">
+                <h1>Dashboard - Analisis de calificacion</h1>
+            </div>
+            <div className="tasasEst">
+
+            </div>
+            <div className="totalesEst">
+
+            </div>
+            <div className="calificacionesEst">
+
+            </div>
+        </div>
+
+        
+    );
+}
+
+export default Estadisticas;
