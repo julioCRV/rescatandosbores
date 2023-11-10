2d666f26c5dd1f51f4c0389992be49437916b637
harol <harolreal4444@gmail.com>
2023-11-10 03:25:10 -0400

    Add: agregando icon like

--- src/components/MostrarPlatillos/components/Recipe.jsx
+++ src/components/MostrarPlatillos/components/Recipe.jsx
@@ -12,13 +12,15 @@ import {Img} from 'react-image';
 import { Link } from 'react-router-dom';
 import { Content } from 'antd/es/layout/layout';
 import '../../sprint2/NavNavegacion/headerNav.css'
-
+import Icon, { HeartOutlined } from '@ant-design/icons';
+import "../../sprint2/MenuItem/MenuItem"
 
 const uri = 'http://18.116.106.247:3000/media/';
 
 
 const Recipe = () => {
     const {id} = useParams();
+    const [likeClick, setLikeClick] = useState(false)
   const [platilloData, setPlatilloData] = useState({
     nombre: '',
     descripcion: '',
@@ -27,7 +29,18 @@ const Recipe = () => {
     identificador: '',
   });
   const [platillos, setPlatillos] = useState([]);
+  const [administrador, setadministrador] = useState(false)
 
+  const like = ()=>{
+    setLikeClick(!likeClick);
+  }
+
+  const HeartSvg = () => (
+    <svg width="24px" height="24px" fill="currentColor" viewBox="0 0 1024 1024">
+      <path d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
+    </svg>
+  );
+  const HeartIcon = (props) => <Icon component={HeartSvg} {...props} />;
   useEffect(() => {
     console.log('realizando llamada');
     axios.get(`http://18.116.106.247:3000/mostrarPlatillos/page/${id}`)
@@ -66,14 +79,19 @@ console.log(platilloData.imagen)
               <p>{platilloData.descripcion}</p>
             </div>
             <div className="recipe-buttons">
-            <div className='buttonn'>
+            {!likeClick ? <HeartOutlined className="classHeart " onClick={like}/> : <HeartIcon onClick={like} style={{color:"red",}} className="classHeartLike"/>}
+            {//cambiar por admistrador para ver usuario final
+            }
+            {!administrador &&            <div className='buttonn'>
               <Link to={`/editar-platillo/${id}`}>
                 <Button type="primary" onClick={() => console.log('Editar')}>
                 <EditOutlined />
                 </Button>
             </Link>
-            </div>
             <ModalConfirmation id={platilloData.identificador} nombre={platilloData.nombre} />
+            </div>}
+ 
+
           </div>
           </div>
         <div className='recipe-video'>
--- src/components/sprint2/MenuItem/MenuItemLista.jsx
+++ src/components/sprint2/MenuItem/MenuItemLista.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import './MenuItem.css'
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
-
+import  Icon, {HeartOutlined} from "@ant-design/icons";
 
 function MenuItem({ image, name, id}) {
   const [hoverTitulo, setHoverTitulo] = useState(false);
+  const [likeClick, setLikeClick] = useState(false)
 
   const hoverTrue=()=>{
     setHoverTitulo(true);
@@ -13,8 +14,18 @@ function MenuItem({ image, name, id}) {
   const hoverFalse = () => {
     setHoverTitulo(false);
   }
+  const like = ()=>{
+    setLikeClick(!likeClick);
+  }
+  const HeartSvg = () => (
+    <svg width="24px" height="24px" fill="currentColor" viewBox="0 0 1024 1024">
+      <path d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
+    </svg>
+  );
+  const HeartIcon = (props) => <Icon component={HeartSvg} {...props} />;
   
 const classImagen = "menuItemImagen " + `${hoverTitulo ? "menuItemTituloHover" :""}`
+
 const urlImagen = 'http://18.116.106.247:3000/media/imagen/' + image.replace(/ /g, "%20")
   return (
     <div className="menuItem">
@@ -23,11 +34,16 @@ const urlImagen = 'http://18.116.106.247:3000/media/imagen/' + image.replace(/ /
         style={{ backgroundImage: `url(${urlImagen})` }}> 
         </div>
       </Link>
-      <Link to={`/mostrar-platillo/page/${id}`} className="ItemContenedorTitulo">
-        <h3 className="menuItemTitulo"
-        onMouseEnter={hoverTrue}
-        onMouseLeave={hoverFalse}> {name} </h3>
-      </Link>
+     <div className="contenedorTitulo">
+      <Link to={`/mostrar-platillo/page/${id}`} className="ItemContenedorTitulo" state={{likeClick, setLikeClick, like}}>
+          <h3 className="menuItemTitulo"
+          onMouseEnter={hoverTrue}
+          onMouseLeave={hoverFalse}> {name} </h3>
+          
+        </Link>
+      
+      {!likeClick ? <HeartOutlined className="classHeart " onClick={like}/> : <HeartIcon onClick={like} style={{color:"red",}} className="classHeartLike"/>}
+     </div>
   </div>
   );
 }
