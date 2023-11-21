0c82d272fb7d664efc9181d48a91bb60910916d1
julioCRV <julioroquevalda6@gmail.com>
2023-11-21 16:32:39 -0400

    actualización con la rama main del respositorio

--- src/Nueva carpeta/components/MenuNavegacion/header.jsx
+++
@@ -1,9 +0,0 @@
-import "./header.css";
-
-const header = () => {
-    return <div class = "headerA">
-        <img class="imgA" src="/src/assets/logo.png" alt="logo" />
-        <img class="imgB" src="/src/assets/banner.jpeg" alt="logo" />
-    </div>
-}
-export default header;
